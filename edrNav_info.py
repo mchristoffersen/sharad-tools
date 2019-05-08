723063c0f30dafc679183bedca34fad7432aea1c
tobers.b <btober@arizona.edu>
2019-05-08 09:20:36 -0700

    Update edrNav_info.py

--- edrNav_info.py
+++ edrNav_info.py
@@ -28,32 +28,32 @@ for root, dirs, files in os.walk(in_path):
                 print(runName)
                 print('--------------------------------')        
 
-                try:
-                    # read lbl files and parse info
-                    lblDic = lbl_Parse(lbl_path)
-                    records = lblDic['FILE_RECORDS']
+            # try:
+                # read lbl files and parse info
+                lblDic = lbl_Parse(lbl_path)
+                records = lblDic['FILE_RECORDS']
 
-                    # read aux file and parse info  
-                    auxDF = aux_Parse(aux_path)
+                # read aux file and parse info  
+                auxDF = aux_Parse(aux_path)
 
-                    # pre-allocate empty numpy array to hold nav data
-                    navDat = np.zeros((records,5))
+                # pre-allocate empty numpy array to hold nav data
+                navDat = np.zeros((records,5))
 
-                    navDat[:,0] = int(runName.split('_')[1] + runName.split('_')[2])
-                    navDat[:,1] = int(np.arange(records))[:]
-                    navDat[:,2] = auxDF['SUB_SC_PLANETOCENTRIC_LATITUDE'][:]
-                    navDat[:,3] = auxDF['SUB_SC_EAST_LONGITUDE'][:]
-                    navDat[:,4] = auxDF['SOLAR_ZENITH_ANGLE'][:]
+                navDat[:,0] = int(runName.split('_')[1] + runName.split('_')[2])
+                navDat[:,1] = int(np.arange(records))[:]
+                navDat[:,2] = auxDF['SUB_SC_PLANETOCENTRIC_LATITUDE'][:]
+                navDat[:,3] = auxDF['SUB_SC_EAST_LONGITUDE'][:]
+                navDat[:,4] = auxDF['SOLAR_ZENITH_ANGLE'][:]
 
-                    # save data
-                    np.savetxt(out_path + runName.split('_')[1] + '_' + runName.split('_')[2] + '_nav.csv', navDat, delimiter = ',', newline = '\n', fmt ='%s')
-                
-                except Exception as err:
-                    print('--------------------------------')  
-                    print(err)
-                    bad_files.append(runName)
-                    print(runName)
-                    print('--------------------------------')  
+                # save data
+                np.savetxt(out_path + runName.split('_')[1] + '_' + runName.split('_')[2] + '_nav.csv', navDat, delimiter = ',', newline = '\n', fmt ='%s')
+            
+            # except Exception as err:
+                print('--------------------------------')  
+                print(err)
+                bad_files.append(runName)
+                print(runName)
+                print('--------------------------------')  
 
 try:
     print('Saving list of bad files')
