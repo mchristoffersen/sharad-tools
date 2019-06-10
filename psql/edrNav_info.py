3cc60ba16ef389f9a410e3b208cfaed28ba47005
colonel-toby <tober@lpl.arizona.edu>
2019-06-10 13:38:13 -0700

    solar_long added to edr_nav

--- psql/edrNav_info.py
+++ psql/edrNav_info.py
@@ -42,13 +42,14 @@ for root, dirs, files in os.walk(in_path):
                     auxDF = aux_Parse(aux_path)
 
                     # pre-allocate empty numpy array to hold nav data
-                    navDat = np.zeros((records,5))
+                    navDat = np.zeros((records,6))
 
                     navDat[:,0] = int(runName.split('_')[1] + runName.split('_')[2])
                     navDat[:,1] = list(np.arange(records))
                     navDat[:,2] = auxDF['SUB_SC_PLANETOCENTRIC_LATITUDE'][:]
                     navDat[:,3] = auxDF['SUB_SC_EAST_LONGITUDE'][:]
                     navDat[:,4] = auxDF['SOLAR_ZENITH_ANGLE'][:]
+                    navDat[:,5] = auxDF['SOLAR_LONGITUDE'][:]
 
                     # save data
                     np.savetxt(out_path + runName.split('_')[1] + '_' + runName.split('_')[2] + '_nav.csv', navDat, delimiter = ',', newline = '\n', fmt ='%s')
