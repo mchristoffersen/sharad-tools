cf0bbf6d12eea9f4ecf71952fd2a5dd8358fde49
Micascisto <sn.stefano@gmail.com>
2022-06-10 09:19:02 -0500

    Created and tested findSurface

---
+++ findSurface/findSurface.bash
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# Wrapper for findSurface.py
+#
+# Written by Stefano Nerozzi, last edit 09-Jun-2022
+#
+# To run this script:
+# ./findSurface.bash -a <study area name> -w <window size> -s <smoothing width> -l <profile list> -r <PDS .IMG dir path> -n <nav file dir path> -c <number of cpu>
+#
+# Read instruction file for detailed explanations
+
+if [ ! ${1} ]; then
+  echo "Fatal error! You didn't read the README, did you?"
+  exit 0
+fi
+
+while getopts ":a:w:s:l:r:n:c:" opt; do
+  case $opt in
+    a)
+      study_area="$OPTARG"
+      ;;
+    w)
+      window_size="$OPTARG"
+      ;;
+    s)
+      smoothing_width="$OPTARG"
+      ;;
+    l)
+      profile_list="$OPTARG"
+      ;;
+    r)
+      rad_dir_path="$OPTARG"
+      ;;
+    n)
+      nav_dir_path="$OPTARG"
+      ;;
+    c)
+      ncpu="$OPTARG"
+      ;;
+    \?)
+      echo "You didn't read the README, did you?"
+      exit 1
+      ;;
+  esac
+done
+
+# Check things with user before making disasters
+echo
+echo "findSurface by Stefano Nerozzi"
+echo
+echo "Your output will be in: $PWD/${study_area}/"
+echo "The window size is: ${window_size} samples"
+echo "The smoothing width is: ${smoothing_width} traces"
+echo "This script will search for .IMG files in: ${rad_dir_path}"
+echo "his script will search for nav files in: ${nav_dir_path}"
+echo "You want to use ${ncpu} cpu threads"
+echo
+read -p "Before proceeding, is this correct? [Y/n] " -r
+if [[ $REPLY =~ ^[Nn]$ ]]; then
+  echo "Fucking bananas!"
+  exit
+fi
+
+# The bash magic begins
+mkdir -p ${study_area}
+while read profile; do
+  profile=${profile#*_}  # Extract the profile number after s_ or S_
+	rad_path=$(find ${rad_dir_path} -type f -name "*${profile}_RGRAM.IMG")
+	nav_path=$(find ${nav_dir_path} -type f -name "*${profile}_geom_nadir.csv")
+	echo "python findSurface.py ${study_area} ${window_size} ${smoothing_width} ${rad_path} ${nav_path}" >> tmp_command_list
+done < ${profile_list}
+
+# Use GNU Parallel to run the Python
+parallel -j ${ncpu} --no-notice < tmp_command_list
+
+# Cleanup
+rm -f tmp_command_list
+
+echo "All done!"
---
+++ findSurface/findSurface.py
@@ -0,0 +1,135 @@
+import os,sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from PIL import Image
+
+'''
+Script to ingest a simc nadir nav file and refine the surface reflector sample location by looking for the max power return.
+The output format is identical to the simc nav file.
+Note: the sample location won't always correspond to the nadir return.
+
+python findSurface.py [study_area] [window_size] [smoothing_width] [rad_path] [nav_path]
+
+[study_area]: name of the study area, no spaces
+[window_size]: desired window size (# of samples), centered on the simc predicted nadir surface return (20-30 are good values)
+[smoothing_width]: desired smoothing window width (10-20 are good values)
+[rad_path]: path to an archive of radargrams in the PDS .IMG format (e.g., S_01294501_RGRAM.IMG)
+[nav_path]: path to the simc-produced nadir nav files (e.g., s_01294501_geom_nadir.csv)
+
+Author: Stefano Nerozzi
+Created: 08/Jun/2022
+Updated: 10/Jun/2022
+'''
+
+# Read and set up arguments
+study_area = sys.argv[1]
+window = int(sys.argv[2])
+mavg_size = int(sys.argv[3])
+rad_path = sys.argv[4]
+nav_path = sys.argv[5]
+
+# Initialize some constants and parameters
+sample_size = 37.5e-9  # 37.5 ns
+speedlight = 29979245  # Unicorns
+r = 3600  # Rows = samples in a trace
+c = 0  # Columns = Traces in a radargram
+profile = rad_path.split('/')[-1]  # Extract file name
+profile = profile.split('_')[1]  # Extract profile number
+window_narrow = int(mavg_size/2)  # Second pass window size
+
+
+# Read the radargram
+data = np.fromfile(rad_path, dtype="float32")
+c = len(data)//r  # Calculate number of traces
+data = np.reshape(data, (r, c))  # Reshape data into a typical radargram
+data_dB = 10 * np.log10(data)  # Turn radargram amplitude into power (dB)
+
+
+# Read the simc nav file
+nav_data = pd.read_csv(nav_path, sep = ',')  # Read Lat, Lon, predicted nadir surface location from simc nav output file
+nad_sample = nav_data['sample'].astype(int)  # Get predicted nadir sample number
+
+
+# Find max power surface return near predicted nadir location
+maxp_wind_init = np.zeros(((window//2)*2,c))  # Initialize the first pass search window, corrects odd values
+maxp_wind_final = np.zeros(((window_narrow//2)*2,c))  # Initialize the second pass search window, correct odd values
+sample_maxp_init = np.zeros(c)  # Initialize max power sample location array, initial pass
+sample_maxp_smooth = np.zeros(c)  # Initialize max power sample location array, smoothed by moving avg
+sample_maxp_final = np.zeros(c)  # Initialize max power sample location array, final pass
+new_sample = np.zeros(c)
+tmp = 0
+
+try:
+    for i in range(c):  # Cycle through every trace
+        if (nad_sample[i] - (window // 2) < 0):  # Case where surface is near the top of the radargram
+            maxp_wind_init[:,i] = data_dB[0 : (nad_sample[i] + (window // 2)),i]
+        elif (nad_sample[i] + (window // 2) > r):  # Case where surface is near the bottom of the radargram
+            maxp_wind_init[:,i] = data_dB[(nad_sample[i] - (window // 2)) : r]
+        else: # Case where window does not reach top or bottom of the radargram
+            maxp_wind_init[:,i] = data_dB[(nad_sample[i] - (window // 2)) : (nad_sample[i] + (window // 2)),i]
+
+    sample_maxp_init = nad_sample + np.argmax(maxp_wind_init, axis = 0) - (window // 2)  # Find actual location of max power samples
+
+    sample_maxp_smooth = np.convolve(sample_maxp_init, np.ones((mavg_size,))/mavg_size, mode='same').astype(int)  # Moving average to smooth out the jitter
+    for i in range(mavg_size//2):  # Correct the first and last mavg_size/2 samples because they were averaged to zeroes
+        sample_maxp_smooth[i] = (sample_maxp_smooth[i] * (mavg_size/(mavg_size//2+i))).astype(int)  # First mavg_size/2 samples
+        sample_maxp_smooth[(c-i-1)] = (sample_maxp_smooth[(c-i-1)] * (mavg_size/(mavg_size//2+i+1))).astype(int)  # Last mavg_size/2 samples
+
+    for i in range(c):  # Cycle through every trace a second time with a narrow window
+        if (sample_maxp_smooth[i] - (window_narrow // 2) < 0):  # Case where surface is near the top of the radargram
+            maxp_wind_final[:,i] = data_dB[0 : (sample_maxp_smooth[i] + (window_narrow // 2)),i]
+        elif (sample_maxp_smooth[i] + (window_narrow // 2) > r):  # Case where surface is near the bottom of the radargram
+            maxp_wind_final[:,i] = data_dB[(sample_maxp_smooth[i] - (window_narrow // 2)) : r]
+        else: # Case where window does not reach top or bottom of the radargram
+            maxp_wind_final[:,i] = data_dB[(sample_maxp_smooth[i] - (window_narrow // 2)) : (sample_maxp_smooth[i] + (window_narrow // 2)),i]
+
+    sample_maxp_final = sample_maxp_smooth + np.argmax(maxp_wind_final, axis = 0) - (window_narrow // 2)  # Find actual location of max power samples
+
+except Exception as err:
+    print(err)
+    print('Fucking bananas!')
+
+nav_data['sample'] = sample_maxp_final  # Substitute sample locations
+
+
+# Export simc-like nav file with new sample locations
+nav_data['lat'] = nav_data['lat'].map(lambda x: '%.6f' % x)  # Keep original float precision because pandas is stupid
+nav_data['lon'] = nav_data['lon'].map(lambda x: '%.6f' % x)  # Keep original float precision because pandas is stupid
+nav_data['elev_IAU2000'] = nav_data['elev_IAU2000'].map(lambda x: '%.3f' % x)  # Keep original float precision because pandas is stupid
+nav_data.to_csv(study_area + '/s_' + profile + '_geom_nadir_maxp.csv', index = False)
+
+
+# The following code is for test purposes
+
+maxPow = np.argmax(data_dB, axis = 0)  # Find max power in each trace
+noise_floor = np.median(data_dB[:50,:])  # Define a noise floor from average power of flattened first 50 rows
+rangedB = data_dB - noise_floor  # Scale image array by max pixel to create jpg output with fret index
+maxdB = np.amax(rangedB, axis = 0)
+ampScale = rangedB / maxdB * 255
+ampScale[np.where(ampScale < 0)] = 0.
+ampScale[np.where(ampScale > 255)] = 255.
+
+imarray = np.zeros((r,c,3), 'uint8')  # Create empty surf index and power arrays
+imarray[:,:,0] = imarray[:,:,1] = imarray[:,:,2] = ampScale[:,:]  # Create surf index image - show scaled radargram as base
+
+# Indicate first pass max power along track as blue
+imarray[sample_maxp_init, np.arange(c),0] = 0
+imarray[sample_maxp_init, np.arange(c),1] = 0
+imarray[sample_maxp_init, np.arange(c),2] = 255
+
+# Indicate second pass max power along track as red
+imarray[sample_maxp_final, np.arange(c),0] = 255
+imarray[sample_maxp_final, np.arange(c),1] = 0
+imarray[sample_maxp_final, np.arange(c),2] = 0
+
+# Indicate nadir position as yellow
+imarray[nad_sample, np.arange(c),0] = 255  
+imarray[nad_sample, np.arange(c),1] = 255
+imarray[nad_sample, np.arange(c),2] = 0
+
+im = Image.fromarray(imarray)
+im.save(study_area + "/" + profile +".png")
+
+
+print("Done processing profile " + profile)
